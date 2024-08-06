57d44b73524708d8fd72968cd5b0ef1e94051ada
Laura <laura.e.vollmer@gmail.com>
2024-08-06 10:07:48 -0500

    setup the components correctly and localhost running successfully

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Money from '../assets/image-money.jpg'
+import Money from '../assets/image-currency.jpg'
 import Dinner from "../assets/image-restaurant.jpg";
 import Plane from "../assets/image-plane.jpg";
 import Confetti from "../assets/image-confetti.jpg";
--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import iconBudgeting from "../assets/icon-budgeting.svg";
 import iconOnboarding from "../assets/icon-onboarding.svg";
 import iconApi from "../assets/icon-api.svg";
-import iconCalendar from "../assets/icon-calendar.svg";
+import iconOnline from "../assets/icon-online.svg";
 
 const Why = () => {
   return (
