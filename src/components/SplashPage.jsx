b679aee85a3f714d4e3e89ab39211f4665f4a82c
Laura <laura.e.vollmer@gmail.com>
2024-08-06 10:06:35 -0500

    updating App jsx with components

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import introDesktop from "../assets/bg-intro-desktop.svg";
+import introMobile from "../assets/bg-intro-mobile.svg";
 
 const SplashPage = () => {
   return (
