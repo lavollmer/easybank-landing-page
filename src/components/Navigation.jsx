834076b8c77971006cdd6df5a9ddae62c1916c77
Laura <laura.e.vollmer@gmail.com>
2024-08-06 10:15:25 -0500

    updating my Navigation bar for desktop and mobile

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 import Hamburger from "../assets/icon-hamburger.svg";
 import Logo from "../assets/logo.svg";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <div className="font-public-sans">
+      <div className="md:hidden flex flex-row justify-between items-center p-6 bg-white">
+        <div>
+          <img src={Logo} alt="Easybank logo" />
+        </div>
+        <div>
+          <img src={Hamburger} alt="Hamburger menu" />
+        </div>
+      </div>
+      <div className="hidden md:flex flex-row justify-between items-center bg-white font-public-sans p-6">
+        <div>
+          <img src={Logo} alt="Easybank logo" />
+        </div>
+        <div className="flex flex-row space-x-2">
+          <h1>Home</h1>
+          <h1>About</h1>
+          <h1>Contact</h1>
+          <h1>Blog</h1>
+          <h1>Careers</h1>
+        </div>
+        <div>
+          <button className="bg-lime-green rounded-full">Request Invite</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navigation
+export default Navigation;
