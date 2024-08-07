57954a1284ee707b44a13bac91270435782e1ba8
Laura <laura.e.vollmer@gmail.com>
2024-08-07 11:22:08 -0500

    updating the navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,16 +13,16 @@ const Navigation = () => {
           <img src={Hamburger} alt="Hamburger menu" />
         </div>
       </div>
-      <div className="hidden md:flex flex-row justify-between items-center bg-white font-public-sans p-6">
+      <div className="hidden md:flex flex-row justify-between items-center bg-white font-public-sans p-8">
         <div>
           <img src={Logo} alt="Easybank logo" />
         </div>
-        <div className="flex flex-row space-x-2 text-grayish-blue">
-          <h1>Home</h1>
-          <h1>About</h1>
-          <h1>Contact</h1>
-          <h1>Blog</h1>
-          <h1>Careers</h1>
+        <div className="flex flex-row space-x-2 text-grayish-blue cursor-pointer">
+          <h1 className="hover:border-b-2 hover:border-lime-green">Home</h1>
+          <h1 className="hover:border-b-2 hover:border-lime-green">About</h1>
+          <h1 className="hover:border-b-2 hover:border-lime-green">Contact</h1>
+          <h1 className="hover:border-b-2 hover:border-lime-green">Blog</h1>
+          <h1 className="hover:border-b-2 hover:border-lime-green">Careers</h1>
         </div>
         <div>
           <button className="bg-gradient-to-r to-blue-300 from-green-200 rounded-full text-white cursor-pointer p-2 px-6">Request Invite</button>
