ac0b1e9db21a66a0386da59843d579e33eba7b88
Laura <laura.e.vollmer@gmail.com>
2024-08-07 11:27:20 -0500

    updating splash page

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 import introDesktop from "../assets/bg-intro-desktop.svg";
 import introMobile from "../assets/bg-intro-mobile.svg";
 
 const SplashPage = () => {
   return (
-    <div>SplashPage</div>
-  )
-}
+    <div className="bg-very-light-gray font-public-sans">
+      <div>
+        <div>
+          <h1 className="text-dark-blue text-xl">
+            Next generation of digital banking
+          </h1>
+        </div>
+        <div>
+          <p className="text-grayish-blue">
+            Take your financial life online. Your Easybank account will be a
+            one-stop-shop for spending, saving, budgeting, investing, and much
+            more.
+          </p>
+        </div>
+        <div>
+          <button className="bg-gradient-to-r to-blue-300 from-green-200 rounded-full text-white cursor-pointer p-2 px-6">
+            Request Invite
+          </button>
+        </div>
+      </div>
+      <div>
+        <img src={introDesktop} alt="intro desktop image" />
+      </div>
+    </div>
+  );
+};
 
-export default SplashPage
+export default SplashPage;
