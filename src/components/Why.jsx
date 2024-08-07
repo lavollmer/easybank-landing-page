de250c461102a25927c06fc06b5915307aad60de
Laura <laura.e.vollmer@gmail.com>
2024-08-07 14:45:37 -0500

    updating the Why EasyBank component

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import iconBudgeting from "../assets/icon-budgeting.svg";
 import iconOnboarding from "../assets/icon-onboarding.svg";
 import iconApi from "../assets/icon-api.svg";
@@ -6,8 +6,30 @@ import iconOnline from "../assets/icon-online.svg";
 
 const Why = () => {
   return (
-    <div>Why</div>
-  )
-}
+    <div className="font-public-sans bg-light-grayish-blue">
+      <div>
+        <h1 className="text-dark-blue text-2xl">Why choose Easybank?</h1>
+        <p className="text-grayish-blue">
+          We leverage Open Banking to turn your bank account into your financial
+          hub. Control your finances like never before.
+        </p>
+      </div>
+      <div className="flex flex-row">
+        <div className="flex flex-col items-center justify-center">
+          <div>
+            <img src={iconOnline} alt="icon online" />
+          </div>
+          <div>
+            <h2 className="text-dark-blue text-lg font-bold">Online Banking</h2>
+            <p className="text-grayish-blue">
+              Our modern web and mobile applications allow you to keep track of
+              your finances wherever you are in the world.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Why
+export default Why;
