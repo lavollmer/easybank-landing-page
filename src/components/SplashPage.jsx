e73d34aac8bdef82cb3c4c21ab0d887e7557febd
Laura <laura.e.vollmer@gmail.com>
2024-08-07 12:39:25 -0500

    styling the splash page text

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import introDesktop from "../assets/bg-intro-desktop.svg";
 import introMobile from "../assets/bg-intro-mobile.svg";
+import imageMockups from "../assets/image-mockups.png";
 
 const SplashPage = () => {
   return (
-    <div className="bg-very-light-gray font-public-sans">
-      <div>
-        <div>
-          <h1 className="text-dark-blue text-xl">
+    <div className="flex flex-row z-0 bg-very-light-gray font-public-sans">
+      <div className="flex flex-col justify-center">
+        <div className="flex items-center text-left">
+          <h1 className="text-dark-blue text-5xl">
             Next generation of digital banking
           </h1>
         </div>
         <div>
-          <p className="text-grayish-blue">
+          <p className="text-grayish-blue text-2xl">
             Take your financial life online. Your Easybank account will be a
             one-stop-shop for spending, saving, budgeting, investing, and much
             more.
           </p>
         </div>
         <div>
-          <button className="bg-gradient-to-r to-blue-300 from-green-200 rounded-full text-white cursor-pointer p-2 px-6">
+          <button className="bg-gradient-to-r to-blue-300 from-green-200 rounded-full text-white text-xl cursor-pointer p-2 py-4 px-8">
             Request Invite
           </button>
         </div>
       </div>
-      <div>
-        <img src={introDesktop} alt="intro desktop image" />
+      <div className="flex flex-col relative">
+        <img src={introDesktop} alt="intro desktop image" className="absolute z-0 h-10 w-10" />
+        <img src={imageMockups} alt="intro desktop image" className="absolute z-10 h-10 w-10" />
       </div>
     </div>
   );
