3c64db762a4e5fea2664e2371f49f83edbd37dfd
Laura <laura.e.vollmer@gmail.com>
2024-08-08 09:38:20 -0500

    updating the easybanking section for mobile

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -6,15 +6,27 @@ import iconOnline from "../assets/icon-online.svg";
 
 const Why = () => {
   return (
-    <div className="font-public-sans bg-light-grayish-blue p-20">
+    <div className="flex flex-col items-center text-center font-public-sans bg-light-grayish-blue p-10">
       <div>
-        <h1 className="text-dark-blue text-2xl">Why choose Easybank?</h1>
+        <h1 className="text-dark-blue text-3xl mb-4">Why choose Easybank?</h1>
         <p className="text-grayish-blue">
           We leverage Open Banking to turn your bank account into your financial
           hub. Control your finances like never before.
         </p>
       </div>
-      <div className="flex">
+      <div className="flex md:flex-row items-center justify-center">
+        <div className="flex flex-col md:flex-row items-center justify-center space-y-8 mt-10">
+          <div>
+            <img src={iconOnline} alt="icon online" />
+          </div>
+          <div className="space-y-6">
+            <h2 className="text-dark-blue text-2xl">Online Banking</h2>
+            <p className="text-grayish-blue">
+              Our modern web and mobile applications allow you to keep track of
+              your finances wherever you are in the world.
+            </p>
+          </div>
+        </div>
         <div className="flex flex-col items-center justify-center">
           <div>
             <img src={iconOnline} alt="icon online" />
@@ -26,47 +38,29 @@ const Why = () => {
               your finances wherever you are in the world.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <div>
-              <img src={iconOnline} alt="icon online" />
-            </div>
-            <div>
-              <h2 className="text-dark-blue text-lg font-bold">
-                Online Banking
-              </h2>
-              <p className="text-grayish-blue">
-                Our modern web and mobile applications allow you to keep track
-                of your finances wherever you are in the world.
-              </p>
-            </div>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div>
+            <img src={iconOnline} alt="icon online" />
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <div>
-              <img src={iconOnline} alt="icon online" />
-            </div>
-            <div>
-              <h2 className="text-dark-blue text-lg font-bold">
-                Online Banking
-              </h2>
-              <p className="text-grayish-blue">
-                Our modern web and mobile applications allow you to keep track
-                of your finances wherever you are in the world.
-              </p>
-            </div>
+          <div>
+            <h2 className="text-dark-blue text-lg font-bold">Online Banking</h2>
+            <p className="text-grayish-blue">
+              Our modern web and mobile applications allow you to keep track of
+              your finances wherever you are in the world.
+            </p>
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <div>
-              <img src={iconOnline} alt="icon online" />
-            </div>
-            <div>
-              <h2 className="text-dark-blue text-lg font-bold">
-                Online Banking
-              </h2>
-              <p className="text-grayish-blue">
-                Our modern web and mobile applications allow you to keep track
-                of your finances wherever you are in the world.
-              </p>
-            </div>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div>
+            <img src={iconOnline} alt="icon online" />
+          </div>
+          <div>
+            <h2 className="text-dark-blue text-lg font-bold">Online Banking</h2>
+            <p className="text-grayish-blue">
+              Our modern web and mobile applications allow you to keep track of
+              your finances wherever you are in the world.
+            </p>
           </div>
         </div>
       </div>
