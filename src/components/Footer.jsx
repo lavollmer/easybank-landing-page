e8c018e151bd24b55f849d1f590370441715f041
Laura <laura.e.vollmer@gmail.com>
2024-08-09 10:23:38 -0500

    updating the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,33 +7,40 @@ import Twitter from "../assets/icon-twitter.svg";
 import Youtube from "../assets/icon-youtube.svg";
 import Pinterest from "../assets/icon-pinterest.svg";
 import Instagram from "../assets/icon-instagram.svg";
+import LogoWhite from "../assets/easybanklogo.png";
 
 const Footer = () => {
   return (
-    <div className="bg-dark-blue">
-      <div>
-        <img src={Logo} alt="logo" />
+    <div className="flex flex-row bg-dark-blue font-public-sans text-white justify-between p-20">
+      <div className="flex flex-col space-y-10">
+        <div>
+          <img src={LogoWhite} alt="logo" />
+        </div>
+        <div className="flex flex-row space-x-4">
+          <img src={Facebook} alt="facebook" />
+          <img src={Twitter} alt="twitter" />
+          <img src={Youtube} alt="youtube" />
+          <img src={Pinterest} alt="pinterest" />
+          <img src={Instagram} alt="instagram" />
+        </div>
       </div>
-      <div className="flex flex-row space-x-1">
-        <img src={Facebook} alt="facebook" />
-        <img src={Twitter} alt="twitter" />
-        <img src={Youtube} alt="youtube" />
-        <img src={Pinterest} alt="pinterest" />
-        <img src={Instagram} alt="instagram" />
+      <div className="flex flex-row space-x-16 text-lg text-grayish-blue">
+        <div className="flex flex-col space-y-2">
+          <p>About Us</p>
+          <p>Contact</p>
+          <p>Blog</p>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <p>Careers</p>
+          <p>Support</p>
+          <p>Privacy Policy</p>
+        </div>
       </div>
-      <div>
-        <p>About Us</p>
-        <p>Contact</p>
-        <p>Blog</p>
-        <p>Careers</p>
-        <p>Support</p>
-        <p>Privacy Policy</p>
-      </div>
-      <div>
-        <button className="bg-gradient-to-r to-blue-300 from-green-200 rounded-full text-white cursor-pointer p-2 px-6">
+      <div className="p-2 text-right space-y-4">
+        <button className="bg-gradient-to-r to-blue-300 from-green-200 rounded-full font-semibold text-white cursor-pointer p-2 px-6">
           Request Invite
         </button>
-        <p>&copy; Easybank. All Rights Reserved</p>
+        <p className="text-grayish-blue">&copy; Easybank. All Rights Reserved</p>
       </div>
     </div>
   );
