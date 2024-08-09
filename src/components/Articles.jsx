869e09e5e62af8a1fabb3f82e5a1f8cca3943d06
Laura <laura.e.vollmer@gmail.com>
2024-08-09 09:57:30 -0500

    styling the card component

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import Money from '../assets/image-currency.jpg'
+import React from "react";
+import Money from "../assets/image-currency.jpg";
 import Dinner from "../assets/image-restaurant.jpg";
 import Plane from "../assets/image-plane.jpg";
 import Confetti from "../assets/image-confetti.jpg";
+import Card from "./Card.jsx";
 
 const Articles = () => {
   return (
-    <div>Articles</div>
-  )
-}
+    <div className="bg-very-light-gray text-public-sans">
+      <div>
+        <h1 className="text-dark-blue text-5xl p-20">Latest Articles</h1>
+      </div>
+      <div className="card-articles">
+        <Card
+          image={Money}
+          imageDesc="money"
+          author="By Claire Robinson"
+          title="Receive money in any currency with no fees"
+          content="The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single..."
+          className="bg-blue-500 text-white"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Articles
+export default Articles;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Card = () => {
+const Card = ({ title, content, author, image, imageDesc }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="flex flex-col p-10 rounded-lg shadow-lg bg-white">
+      <img
+        src={image}
+        alt={imageDesc}
+        className="h-full w-full object-cover rounded-t-lg"
+      />
+      <div className="p-4">
+        <p className="text-grayish-blue">{author}</p>
+        <h2 className="text-dark-blue text-xl font-semibold">{title}</h2>
+        <p className="text-grayish-blue mt-2">{content}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
