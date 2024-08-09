55fc86ee532edc0bdc4ca0a6a8c37b170de00c2e
Laura <laura.e.vollmer@gmail.com>
2024-08-09 12:26:39 -0500

    adding the sidenav bar functionality

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import Hamburger from "../assets/icon-hamburger.svg";
 import Logo from "../assets/logo.svg";
+import SideNavBar from "./SideNavBar";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="font-public-sans">
       <div className="md:hidden flex flex-row fixed top-0 z-20 w-full justify-between items-center p-6 bg-white">
         <div>
           <img src={Logo} alt="Easybank logo" />
         </div>
+        {/* hamburger button click */}
         <div>
-          <img src={Hamburger} alt="Hamburger menu" />
+          <button>
+            <img src={Hamburger} alt="Hamburger menu" onClick={handleOnClick} />
+          </button>
         </div>
+        {isOpen && (
+          <div>
+            <SideNavBar />
+          </div>
+        )}
       </div>
       <div className="hidden md:flex flex-row fixed top-0 z-20 w-full justify-between items-center bg-white font-public-sans p-8">
         <div>
@@ -25,7 +41,9 @@ const Navigation = () => {
           <h1 className="hover:border-b-2 hover:border-lime-green">Careers</h1>
         </div>
         <div>
-          <button className="bg-gradient-to-r to-blue-400 from-green-400 rounded-full text-white cursor-pointer p-2 px-6 hover:from-blue-400 hover:to-green-300">Request Invite</button>
+          <button className="bg-gradient-to-r to-blue-400 from-green-400 rounded-full text-white cursor-pointer p-2 px-6 hover:from-blue-400 hover:to-green-300">
+            Request Invite
+          </button>
         </div>
       </div>
     </div>
--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const SideNavBar = () => {
   return (
-    <div>SideNavBar</div>
-  )
-}
+    <div className="bg-white text-black text-public-sans">
+      <div>
+        <h1>Home</h1>
+        <h1>About</h1>
+        <h1>Contact</h1>
+        <h1>Blog</h1>
+        <h1>Careers</h1>
+      </div>
+    </div>
+  );
+};
 
-export default SideNavBar
+export default SideNavBar;
